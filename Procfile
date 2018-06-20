c9a0738d9ca30f1a9f212e24a969e138a68bb85b
Maurice <mbanerjeepalmer@gmail.com>
2018-06-21 00:46:59 +0100

    Virtual env and split settings (but not populate).

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn loorollv2.wsgi --log-file -
--- scripts/write.py
+++ scripts/write.py
@@ -7,14 +7,13 @@ def append_html(mimedocument, filename):
     """Convert an email to HTML and write that locally."""
     try:
         html = mimedocument.get_body().get_content()
-        # TODO Write to DB
         with open(filename, 'a', encoding="utf-8") as f:
             #TODO Tidy.
             f.write('\n')
             f.write(html)
             f.write('\n')
     except Exception as exception:
-        # logging etc. needs fixing.
+        # TODO logging etc. needs fixing.
         # In past versions it used the apiclient errors
         print('Fuck. ' + str(exception))
 
