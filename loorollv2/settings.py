c2ffbca1a3e08d5a6bf8991076cb3685ff33b6b2
mbanerjeepalmer <mbanerjeepalmer@gmail.com>
2018-08-17 00:22:29 +0100

    Remove Social Django

--- loorollv2/settings.py
+++ loorollv2/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'loorollv2app',
-    'social_django',
 ]
 
 MIDDLEWARE = [
@@ -73,8 +72,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'social_django.context_processors.backends',
-                'social_django.context_processors.login_redirect',
             ],
         },
     },
