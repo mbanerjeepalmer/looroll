6f43a2ff12285b7e1aeedee563e72e23c2d0b300
mbanerjeepalmer <mbanerjeepalmer@gmail.com>
2018-08-30 00:16:02 +0100

    Attempt logging

--- loorollv2/settings/heroku.py
+++ loorollv2/settings/heroku.py
@@ -10,3 +10,19 @@ DATABASES['default'].update(db_from_env)
 
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
